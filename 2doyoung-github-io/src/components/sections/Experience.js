c152f59c3c47b80327e0ef68bc6eecad0795cb73
2Doyoung <dlehdud1653@kakao.com>
2025-04-16 15:54:31 +0900

    experience complete

--- 2doyoung-github-io/src/components/sections/Experience.js
+++ 2doyoung-github-io/src/components/sections/Experience.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 const ExperienceStyled = styled.div`
     display: flex;
+    margin-bottom: 3rem;
 `;
 
 const CompanyPeriodWrapStyled = styled.div`
@@ -47,48 +48,114 @@ const UlStyled = styled.ul`
     padding-left: 1rem;
 `;
 
+const ProjectSection = (props) => {
+    const { project } = props;
+
+    return (
+        <>
+            <ProjectTitle>{project.title}</ProjectTitle>
+            <ProjectPeriod>{project.period}</ProjectPeriod>
+            <DescriptionText>{project.desc}</DescriptionText>
+            <SkillText>{project.skills}</SkillText>
+            <UlStyled>
+                {project.items.map((item, index) => {
+                    return <li key={index}>{item}</li>
+                })}
+            </UlStyled>
+        </>
+    )
+}
+
+const ExperienceItem = (props) => {
+    const { experience } = props;
+
+    return (
+        <>
+            <ExperienceStyled>
+                <CompanyPeriodWrapStyled>
+                    <CompanyNameStyled>{experience.company}</CompanyNameStyled>
+                    <YearStyled>{experience.period}</YearStyled>
+                </CompanyPeriodWrapStyled>
+                <DescriptionStyled>
+                    {experience.projects?.map((project, index) => {
+                        return <ProjectSection key={index} project={project}></ProjectSection>
+                    })}
+                </DescriptionStyled>
+            </ExperienceStyled>
+        </>
+    )
+}
 
 const experiences = [
     { 
         company: "애드곰인", 
         period: "2024.07 ~ 2025.02",
+        projects: [
+            {
+                title: "애드곰인(광고중개 플랫폼)",
+                period: "2024.07 ~ 2025.02",
+                desc: "광고중개 플랫폼의 기획부터 개발까지 진행",
+                skills: "사용 기술: HTML, CSS, Javascript, JAVA, SpringBoot, MySQL, Thymeleaf, AWS",
+                items: [
+                    "광고상세설명 등록, 광고포트폴리오 등록",
+                    "나이스페이먼츠 결제 연동, 카카오 알림톡 연동",
+                    "1대1 실시간 채팅기능",
+                    "전체 기획, 백엔드, 프론트엔드 개발"
+                ]
+            }
+        ]
     },
     { 
         company: "코리아아이티아카데미대구", 
-        period: "2023.08 ~ 2024.01" 
+        period: "2023.08 ~ 2024.01",
+        projects: [
+            {
+                title: "프로그래밍 언어 JAVA 기초 교육",
+                period: "2023.08 ~ 2024.01",
+                desc: "",
+                skills: "",
+                items: [
+
+                ]
+            }
+        ]
     },
     { 
         company: "(주)아이티팩토리", 
         period: "2020.11 ~ 2023.04",
-        description: (
-            <>
-              <ProjectTitle>HACCP사후관리 - K-HACCP</ProjectTitle>
-              <ProjectPeriod>2022-06-01 ~ 2023-03-01</ProjectPeriod>
-              <DescriptionText>HACCP 사후관리 프로그램 K-HACCP 설계 및 개발</DescriptionText>
-              <SkillText>사용 기술:  HTML, Javascript, jQuery, JAVA, Spring Framework, MySQL</SkillText>
-              <UlStyled>
-                <li>로그인, 회원가입, 관리자, 사용자관리, 컨설턴트 페이지 개발</li>
-                <li>포트원 결제 API 연동으로 프로그램 사용 요금 결제 개발</li>
-              </UlStyled>
-              <ProjectTitle>고객관리프로그램 - 고무신</ProjectTitle>
-              <ProjectPeriod>2021-11-01 ~ 2022-05-01</ProjectPeriod>
-              <DescriptionText>고객관리 프로그램 고무신 설계 및 개발</DescriptionText>
-              <SkillText>사용 기술: HTML, Javascript, jQuery, JAVA, Spring Framework, MariaDB</SkillText>
-              <UlStyled>
-                <li>로그인, 회원가입, 고객 정보 생성, 고객 정보 조회, 고객 정보 수정, 고객 정보 삭제, 홍보물 업로드 페이지 개발</li>
-                <li>알리고 알림톡 API 연동으로 카카오 알림톡 푸시 개발</li>
-              </UlStyled>
-              <ProjectTitle>ERP, MES프로그램 - BTS(Business Total Solution)</ProjectTitle>
-              <ProjectPeriod>2020-11-01 ~ 2021-11-01</ProjectPeriod>
-              <DescriptionText>ERP, MES 프로그램 유지보수 및 요구사항 반영</DescriptionText>
-              <SkillText>사용 기술: HTML, Javascript, jQuery, JAVA, Spring Framework, MariaDB</SkillText>
-              <UlStyled>
-                <li>대용량 ERP 데이터 최적화로 인한 성능 개선</li>
-                <li>실시간 설비 데이터 모니터링 화면 개발</li>
-                <li>IBsheet 플러그인을 이용해 프로그램 유지보수</li>
-              </UlStyled>
-            </>
-          )
+        projects: [
+            {
+                title: "HACCP사후관리 - K-HACCP",
+                period: "2022-06-01 ~ 2023-03-01",
+                desc: "HACCP 사후관리 프로그램 K-HACCP 설계 및 개발",
+                skills: "사용 기술: HTML, Javascript, jQuery, JAVA, Spring Framework, MySQL",
+                items: [
+                  "로그인, 회원가입, 관리자, 사용자관리, 컨설턴트 페이지 개발",
+                  "포트원 결제 API 연동으로 프로그램 사용 요금 결제 개발",
+                ],
+              },
+              {
+                title: "고객관리프로그램 - 고무신",
+                period: "2021-11-01 ~ 2022-05-01",
+                desc: "고객관리 프로그램 고무신 설계 및 개발",
+                skills: "사용 기술: HTML, Javascript, jQuery, JAVA, Spring Framework, MariaDB",
+                items: [
+                  "로그인, 회원가입, 고객 정보 생성, 조회, 수정, 삭제, 홍보물 업로드 페이지 개발",
+                  "알리고 알림톡 API 연동으로 카카오 알림톡 푸시 개발",
+                ],
+              },
+              {
+                title: "ERP, MES프로그램 - BTS(Business Total Solution)",
+                period: "2020-11-01 ~ 2021-11-01",
+                desc: "ERP, MES 프로그램 유지보수 및 요구사항 반영",
+                skills: "사용 기술: HTML, Javascript, jQuery, JAVA, Spring Framework, MariaDB",
+                items: [
+                  "대용량 ERP 데이터 최적화로 인한 성능 개선",
+                  "실시간 설비 데이터 모니터링 화면 개발",
+                  "IBsheet 플러그인을 이용해 프로그램 유지보수",
+                ],
+              },
+        ]
     },
 ];
 
@@ -97,13 +164,7 @@ function Experience(props) {
         <div>
             {experiences.map((experience, index) => {
                 return (
-                    <ExperienceStyled key={index}>
-                        <CompanyPeriodWrapStyled>
-                            <CompanyNameStyled>{experience.company}</CompanyNameStyled>
-                            <YearStyled>{experience.period}</YearStyled>
-                        </CompanyPeriodWrapStyled>
-                        <DescriptionStyled>{experience.description}</DescriptionStyled>
-                    </ExperienceStyled>
+                    <ExperienceItem key={index} experience={experience}></ExperienceItem>
                 )
             })}
         </div>
